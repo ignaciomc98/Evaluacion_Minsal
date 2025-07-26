0434a2c7506803efd19085d5f0104f3fc290cc9a
ignaciomc98 <ignamedi4198@gmail.com>
2025-07-26 02:16:47 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,11 @@
-# app/database.py
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Sube dos niveles hasta user_api/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'test.db')}"
 
-print(f"Usando base de datos en: {SQLALCHEMY_DATABASE_URL}")  # Para debug
+print(f"Usando base de datos en: {SQLALCHEMY_DATABASE_URL}")  
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
